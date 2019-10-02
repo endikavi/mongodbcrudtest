853a44476a382a842c248f7a125aa5af3868fa64
Endikavi <endika.vi@gmail.com>
2019-10-02 19:28:03 +0200

    Added bodyparser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const bodyParser = require('body-parser');
 
 const db = require('./libs/connect');
 
